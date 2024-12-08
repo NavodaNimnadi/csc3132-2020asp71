37cef400ad85afb04e1654825708e09d2c3e956e
NavodaNimnadi <139615090+NavodaNimnadi@users.noreply.github.com>
2024-12-08 13:23:47 +0000

    gettable file

---
+++ 12_06/gettable.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php
+//get the db connection file
+
+require_once 'dbconf.php';
+require_once 'myfunc.php';
+
+
+book($connect);
+
+?>
+
+
+
+</body>
+</html>
